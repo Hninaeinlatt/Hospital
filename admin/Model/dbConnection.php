0acd3fdceb57f9027317d88b6d6e94bea0e50177
Hninaeinlatt <Hninaeinlatt438@gmail.com>
2023-08-28 12:31:34 +0630

    update

---
+++ admin/Model/dbConnection.php
@@ -0,0 +1,12 @@
+<?php
+$hostname = "vr-crarerhel-do-user-14558665-0.b.db.ondigitalocean.com";
+$port = "25060";
+$dbname = "vr_carer";
+$username = "doadmin";
+$password = "AVNS_9Ct4FPx2u0OOgGC533V";
+$pdo = new PDO(
+    "mysql:host=$hostname;port=$port;dbname=$dbname",
+    $username,
+    $password
+);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
